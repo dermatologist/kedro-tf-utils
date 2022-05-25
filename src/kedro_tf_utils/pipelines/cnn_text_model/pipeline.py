6986bfdabb2ce6690310311aa7dbf15735e03e9d
Bell Eapen <github@gulfdoctor.net>
2022-05-25 13:07:47 -0500

    pipeline in progress

--- src/kedro_tf_utils/pipelines/cnn_text_model/pipeline.py
+++ src/kedro_tf_utils/pipelines/cnn_text_model/pipeline.py
@@ -4,7 +4,28 @@ generated using Kedro 0.18.1
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from kedro_tf_text.pipelines.preprocess.nodes import tabular_model
 
+from kedro_tf_utils.pipelines.cnn_text_model.nodes import early_fusion_mm
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([])
+
+
+def create_fusion_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+
+                    node(
+                        tabular_model,
+                        ["tabular_data", "parameters.embedding"],
+                        "text_model",
+                        name="create_text_model"
+                    ),
+                    node(
+                        early_fusion_mm,
+                        ["text_model", "chexnet_model", "parameters.embedding"],
+                        "fusion_model",
+                        name="create_fusion_model"
+                    ),
+
+    ])
