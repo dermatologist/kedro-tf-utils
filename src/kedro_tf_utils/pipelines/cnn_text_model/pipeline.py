a524f6f983880c295a4ade785f7b25bc8383d848
Bell Eapen <github@gulfdoctor.net>
2022-05-25 14:06:51 -0500

    parameterized pipelines

--- src/kedro_tf_utils/pipelines/cnn_text_model/pipeline.py
+++ src/kedro_tf_utils/pipelines/cnn_text_model/pipeline.py
@@ -12,19 +12,20 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([])
 
 
-def create_fusion_pipeline(**kwargs) -> Pipeline:
+def create_fusion_pipeline(datasets={}, **kwargs) -> Pipeline:
     return pipeline([
 
                     node(
                         tabular_model,
-                        ["tabular_data", "parameters"],
-                        "datasetinmemory",
+                        [datasets.get("tabular_data", "tabular_data"), datasets.get("parameters", "parameters")],
+                        datasets.get("datasetinmemory", "datasetinmemory"),
                         name="create_text_model"
                     ),
                     node(
                         early_fusion_mm,
-                        ["datasetinmemory", "chexnet_model", "parameters"],
-                        "fusion_model",
+                        [datasets.get("datasetinmemory", "datasetinmemory"),
+                         datasets.get("chexnet_model","chexnet_model"), datasets.get("parameters", "parameters")],
+                        datasets.get("fusion_model","fusion_model"),
                         name="create_fusion_model"
                     ),
 
