7f633f3995fb8cd232b0d902a5f9f3d9b78db38a
Bell Eapen <github@gulfdoctor.net>
2022-12-18 17:44:42 -0500

    Move multimodal here

--- src/kedro_tf_utils/pipeline_registry.py
+++ src/kedro_tf_utils/pipeline_registry.py
@@ -4,7 +4,7 @@ from typing import Dict
 from kedro.pipeline import Pipeline, pipeline
 
 from kedro_tf_utils.pipelines.fusion.pipeline import create_fusion_pipeline, create_text_fusion_pipeline
-
+from kedro_tf_utils.pipelines.train_mm_simple.pipeline import create_train_pipeline
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -15,5 +15,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
     return {
         "__default__": create_fusion_pipeline(),
         "report": create_text_fusion_pipeline(),
+        "train": create_train_pipeline()
         }
 
---
+++ src/kedro_tf_utils/pipelines/train_mm_simple/pipeline.py
@@ -0,0 +1,30 @@
+"""
+This is a boilerplate pipeline 'train_mm_simple'
+generated using Kedro 0.18.4
+"""
+
+"""
+This is a boilerplate pipeline 'train'
+generated using Kedro 0.18.1
+"""
+
+
+
+
+from kedro.pipeline import Pipeline, node, pipeline
+from kedro_tf_utils.pipelines.train_mm_simple.nodes import train_multimodal
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
+
+
+def create_train_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+
+                    node(
+                        func=train_multimodal,
+                        inputs=["tabular_data", "image_data", "fusion_model"],
+                        outputs="trained_model",
+                        parameters="params:train_model",
+                        name="train_model"
+                    ),
+                    ])
