135e9ef78f05355f438ce489fb0d58af100a0e90
Bell Eapen <github_public@gulfdoctor.net>
2022-12-21 17:15:27 +0000

    add dense layers function

--- src/kedro_tf_utils/pipelines/fusion/nodes.py
+++ src/kedro_tf_utils/pipelines/fusion/nodes.py
@@ -3,7 +3,7 @@ This is a boilerplate pipeline 'fusion'
 generated using Kedro 0.18.1
 """
 import tensorflow as tf
-from keras.layers import Activation, Input, Dense, Flatten, Dropout, Embedding, BatchNormalization
+from keras.layers import Activation, Input, Dense, Flatten, Dropout, Embedding, BatchNormalization, AveragePooling2D
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.layers import concatenate
 from keras import regularizers
@@ -15,6 +15,18 @@ def last_layer_normalized(model):
     last_layer = model.layers[-2].output
     return BatchNormalization()(last_layer)
 
+# https://github.com/artelab/Image-and-Text-fusion-for-UPMC-Food-101-using-BERT-and-CNNs/blob/main/stacking_early_fusion_UPMC_food101.ipynb
+# !Not currently used
+def add_dense_layers(model, parameters):
+    model.add(AveragePooling2D(pool_size=(8, 8), name='AVG_Pooling'))
+    model.add(Dropout(.4, name='Dropout_0.4'))
+    model.add(Flatten(name='Flatten'))
+    model.add(Dense(128, name='Dense_128'))
+
+    # Keep model layers trainable
+    for layer in model.layers:
+        layer.trainable = True
+    return model
 
 def early_fusion_mm(text_model, image_model, parameters):
     text_last_layer = last_layer_normalized(text_model)
