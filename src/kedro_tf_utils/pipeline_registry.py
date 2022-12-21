916e728a853e0738fedfee9dbec58323a66a2106
Bell Eapen <github_public@gulfdoctor.net>
2022-12-21 15:01:37 +0000

    refactor in progress

--- src/kedro_tf_utils/pipeline_registry.py
+++ src/kedro_tf_utils/pipeline_registry.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
 
-from kedro_tf_utils.pipelines.fusion.pipeline import create_fusion_pipeline, create_text_fusion_pipeline
+from kedro_tf_utils.pipelines.fusion.pipeline import create_tabular_pipeline, create_text_fusion_pipeline
 from kedro_tf_utils.pipelines.train_mm_simple.pipeline import create_train_pipeline
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -13,7 +13,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     return {
-        "__default__": create_fusion_pipeline(),
+        "__default__": create_tabular_pipeline(),
         "report": create_text_fusion_pipeline(),
         "train": create_train_pipeline()
         }
--- src/kedro_tf_utils/pipelines/fusion/pipeline.py
+++ src/kedro_tf_utils/pipelines/fusion/pipeline.py
@@ -8,69 +8,55 @@ from kedro_tf_utils.pipelines.cnn_text_model.nodes import create_cnn_model
 from kedro_tf_utils.pipelines.fusion.nodes import early_fusion_mm
 from kedro_tf_text.pipelines.preprocess.nodes import tabular_model, pickle_processed_text, json_processed_text, create_glove_embeddings
 
+from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
+from kedro_tf_text.pipelines.preprocess.pipeline import create_glove_embedding_pipeline, pickle_processed_text_pipeline
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([])
 
 
-def create_fusion_pipeline(datasets={}, **kwargs) -> Pipeline:
+def create_tabular_pipeline(**kwargs) -> Pipeline:
     return pipeline([
 
                     node(
                         tabular_model,
-                        [datasets.get("tabular_data", "tabular_data"),
-                         datasets.get("parameters", "parameters")],
-                        datasets.get("datasetinmemory", "datasetinmemory"),
+                        inputs=["tabular_data", "params:glove_embedding"],
+                        outputs="datasetinmemory",
                         name="create_text_model"
                     ),
                     node(
                         early_fusion_mm,
-                        [datasets.get("datasetinmemory", "datasetinmemory"),
-                         datasets.get("chexnet_model", "chexnet_model"), datasets.get("parameters", "parameters")],
-                        datasets.get("fusion_model", "fusion_model"),
+                        inputs=["datasetinmemory", "chexnet_model", "params:glove_embedding"],
+                        outputs="fusion_model",
                         name="create_fusion_model"
                     ),
 
                     ])
 
-
-def create_text_fusion_pipeline(datasets={}, **kwargs) -> Pipeline:
+def create_fusion_pipeline(**kwargs) -> Pipeline:
     return pipeline([
+        node(
+            create_cnn_model,
+            inputs=["glove_embedding", "params:glove_embedding"],
+            outputs="datasetinmemory",
+            name="create_cnn_model"
+        ),
+        node(
+            early_fusion_mm,
+            inputs=["datasetinmemory", "chexnet_model", "params:glove_embedding"],
+            outputs="fusion_model",
+            name="create_fusion_model"
+        ),
 
-                    node(
-                        pickle_processed_text,
-                        [datasets.get("text_data", "text_data"),
-                         datasets.get("parameters", "parameters")],
-                        datasets.get("processed_text", "processed_text"),
-                        name="pickle_processed_text"
-                    ),
-                    node(
-                        json_processed_text,
-                        [datasets.get("text_data", "text_data"),
-                         datasets.get("parameters", "parameters")],
-                        datasets.get("vocab_json", "vocab_json"),
-                        name="create_vocab"
-                    ),
-                    node(
-                        create_glove_embeddings,
-                        [datasets.get("pretrained_embedding", "pretrained_embedding"),
-                         datasets.get("vocab_json", "vocab_json"), datasets.get("parameters", "parameters")],
-                        datasets.get("glove_embedding", "glove_embedding"),
-                        name="create_glove_embeddings"
-                    ),
-                    node(
-                        create_cnn_model,
-                        [datasets.get("glove_embedding", "glove_embedding"),
-                         datasets.get("parameters", "parameters")],
-                        datasets.get("datasetinmemory", "datasetinmemory"),
-                        name="create_cnn_model"
-                    ),
-                    node(
-                        early_fusion_mm,
-                        [datasets.get("datasetinmemory", "datasetinmemory"),
-                         datasets.get("chexnet_model", "chexnet_model"), datasets.get("parameters", "parameters")],
-                        datasets.get("fusion_model", "fusion_model"),
-                        name="create_fusion_model"
-                    ),
+    ])
 
-                    ])
+# Demonstrates the use of modular pipelines: https://kedro.readthedocs.io/en/stable/nodes_and_pipelines/modular_pipelines.html
+def create_text_fusion_pipeline(**kwargs) -> Pipeline:
+    created_glove = create_glove_embedding_pipeline()
+    created_pickle = pickle_processed_text_pipeline()
+    created_fusion = create_fusion_pipeline()
+    glove_embedding_pipeline = modular_pipeline(pipe=created_glove, parameters={
+        "params:embedding": "params:glove_embedding"})
+    processed_text_pipeline = modular_pipeline(pipe=created_pickle, parameters={
+                                               "params:embedding": "params:glove_embedding"})
+    return processed_text_pipeline + glove_embedding_pipeline + created_fusion
