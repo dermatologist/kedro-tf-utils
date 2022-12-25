7ff87261d259305399f8b66391cc0a9246f6d456
Bell Eapen <github_public@gulfdoctor.net>
2022-12-25 23:58:44 +0000

    remove cnn_text_model from tf_utils (to tf_text)

--- src/kedro_tf_utils/pipelines/fusion/pipeline.py
+++ src/kedro_tf_utils/pipelines/fusion/pipeline.py
@@ -4,11 +4,10 @@ generated using Kedro 0.18.1
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from kedro_tf_utils.pipelines.cnn_text_model.nodes import create_cnn_model
 from kedro_tf_utils.pipelines.fusion.nodes import early_fusion_mm
 
 from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
-from kedro_tf_text.pipelines.preprocess.pipeline import create_glove_embedding_pipeline, pickle_processed_text_pipeline, word2vec_embedding
+from kedro_tf_text.pipelines.preprocess.pipeline import word2vec_embedding
 from kedro_tf_text.pipelines.bert.nodes import get_tf_bert_model
 from kedro_tf_text.pipelines.tabular.nodes import tabular_model
 from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
