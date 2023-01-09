dbb8d205c716c80c0a874b96543e569ac4234ef0
Bell Eapen <github@gulfdoctor.net>
2023-01-09 16:32:11 -0600

    Remove unused import

--- src/kedro_tf_utils/pipelines/fusion/nodes.py
+++ src/kedro_tf_utils/pipelines/fusion/nodes.py
@@ -15,7 +15,6 @@ from keras.layers import (
     Flatten,
 )
 from keras.models import Model
-from kedro_tf_image.extras.classes.tf_image_classifier_model import ImageClassifierModel
 
 
 # https://github.com/keras-team/keras/issues/7403
